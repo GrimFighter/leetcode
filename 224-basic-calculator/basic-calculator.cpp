1ea12a8e23df29a3ea5cad8a0336fb42c3da1215
Sayantan Mondal <75028962+GrimFighter@users.noreply.github.com>
2024-09-20 19:54:12 +0530

    Time: 7 ms (66.49%) | Memory: 9.6 MB (49.41%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int num = 0, ans = 0, sign = 1;
+
+        stack<int> stack{{sign}};
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '(') {
+                stack.push(sign);
+            } else if (c == ')') {
+                stack.pop();
+            } else if (c == '+' || c == '-') {
+                ans += sign * num;
+                sign = (c == '+' ? 1 : -1) * stack.top();
+                num = 0;
+            }
+        }
+        return ans + sign * num;
+    }
+};
