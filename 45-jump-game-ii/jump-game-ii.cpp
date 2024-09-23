b37ff37e381bc8cc8b3c7be183af2adeb4916526
Sayantan Mondal <75028962+GrimFighter@users.noreply.github.com>
2024-09-23 13:35:54 +0530

    Time: 3 ms (98.98%) | Memory: 19.4 MB (40.39%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
+        int jumps = 0, end = 0, far = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            far = max(far, i + nums[i]);
+
+            if (end == nums.size() - 1) {
+                break;
+            }
+            if (i == end) {
+                end = far;
+                ++jumps;
+
+                
+            }
+        }
+        return jumps;
+    }
+};
