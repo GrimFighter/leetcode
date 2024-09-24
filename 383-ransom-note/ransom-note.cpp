62d4adbeb7cc72904200b784ce70f04327aa596a
Sayantan Mondal <75028962+GrimFighter@users.noreply.github.com>
2024-09-24 13:52:18 +0530

    Time: 16 ms (58.87%) | Memory: 11.6 MB (14.53%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int i = 0, j = 0;
-        ranges::sort(ransomNote);
-        ranges::sort(magazine);
+        // int i = 0, j = 0;
+        // ranges::sort(ransomNote);
+        // ranges::sort(magazine);
 
-        for (; i < magazine.length(); ++i) {
-            if (ransomNote[j] == magazine[i]) {
-                ++j;
-            }
+        // for (; i < magazine.length(); ++i) {
+        //     if (ransomNote[j] == magazine[i]) {
+        //         ++j;
+        //     }
+        // }
+
+        // return j == ransomNote.length();
+
+
+        unordered_map<char, int> map;
+
+        for(char c: magazine){
+            ++map[c];
         }
 
-        return j == ransomNote.length();
+        for(char c: ransomNote){
+            if(map[c] <= 0){
+                return false;
+            }
+            --map[c];
+        }
+        return true;
     }
 };
