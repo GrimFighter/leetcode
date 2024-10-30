a40e78089c26aa8272cb186cb15a1e035972c30c
Sayantan Mondal <75028962+GrimFighter@users.noreply.github.com>
2024-10-30 18:04:41 +0530

    Time: 1100 ms (37.27%) | Memory: 25.2 MB (6.18%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,14 +4,36 @@ class Solution:
         def isPalinDrome(s):
             return s == s[::-1]
 
-        result = ""
-        length = 0
+        # result = ""
+        # length = 0
+
+        # for i in range(len(s)):
+        #     for j in range(len(s), i, -1):
+        #         if isPalinDrome(s[i:j]):
+        #             if len(s[i:j]) > length:
+        #                 length = len(s[i:j])
+        #                 result = s[i:j]
+
+        # return result
+        if (len(s)) <= 1:
+            return s
+
+        if isPalinDrome(s):
+            return s
+
+        length = 1
+        result = s[0]
+
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
 
         for i in range(len(s)):
-            for j in range(len(s), i, -1):
-                if isPalinDrome(s[i:j]):
-                    if len(s[i:j]) > length:
-                        length = len(s[i:j])
-                        result = s[i:j]
+            dp[i][i] = True
+            for j in range(i):
+                if s[j] == s[i] and (i - j <= 2 or dp[j + 1][i - 1]):
+                    dp[j][i] = True
+                    if i - j + 1 > length:
+                        length = i - j + 1
+                        result = s[j : i + 1]
 
         return result
+
