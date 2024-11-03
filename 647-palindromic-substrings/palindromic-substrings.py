4adec75905c8a84eb76a01c0f492bfa5297fd56e
Sayantan Mondal <75028962+GrimFighter@users.noreply.github.com>
2024-11-04 03:15:22 +0530

    Time: 117 ms (66.87%) | Memory: 16.6 MB (66.57%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,11 +1,27 @@
 class Solution:
+    # def countSubstrings(self, s: str) -> int:
+    #     count = 0
+    #     for i in range(len(s)):
+    #         count+=1
+    #         for j in range(i):
+    #             if self.isP(s[j:i+1]):
+    #                 count+=1
+    #     return count
+    # def isP(self,s):
+    #     return s == s[::-1]
+
     def countSubstrings(self, s: str) -> int:
         count = 0
+
         for i in range(len(s)):
-            count+=1
-            for j in range(i):
-                if self.isP(s[j:i+1]):
-                    count+=1
+            count += self.expand(s, i, i)
+            count += self.expand(s, i, i + 1)
         return count
-    def isP(self,s):
-        return s == s[::-1]
+
+    def expand(self, s, l, r):
+        count = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            count += 1
+            l -= 1
+            r += 1
+        return count
